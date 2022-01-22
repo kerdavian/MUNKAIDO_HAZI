3445c01d88405c21cae2963b8308713a2755909b
Csampai Elemér <csampaie.5@gmail.com>
2022-01-22 18:15:01 +0100

    Inspirációs kép

--- components/StatusPage.js
+++ components/StatusPage.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { StyleSheet, Text, View, Switch, TouchableOpacity, Image } from 'react-native';
 
 import { loginStatus, signOutUser } from '../auth';
@@ -7,6 +7,14 @@ import { storeUserData, removeUserData } from '../localStorage';
 
 export default function StatusPage(props) {
   // const [currentStatus, setCurrentStatus] = useState(false);
+  const [link, setLink] = useState();
+
+  const generateImage = async () => {
+    const response = await fetch('https://inspirobot.me/api?generate=true');
+    const data = await response.text();
+    setLink(data);
+    // return data;
+  };
 
   const toggleSwitch = () => {
     const newState = props.toggleUserState();
@@ -14,6 +22,7 @@ export default function StatusPage(props) {
     // visszatérési értékét használjuk!
     saveHistoryOnFirebase(props.userData.email, newState);
     // setCurrentStatus(previousState => !previousState);
+    generateImage();
   };
 
   const handleLogout = async () => {
@@ -29,6 +38,7 @@ export default function StatusPage(props) {
       await storeUserData(userData);
       console.log(`${userData.name} received when innerpage loaded`);
       props.setUserData(userData);
+      generateImage();
     })();
   }, []);
 
@@ -55,6 +65,9 @@ export default function StatusPage(props) {
         style={[styles.button, styles.shadow]}>
         <Text style={styles.buttonText}>Napló megtekintése</Text>
       </TouchableOpacity>
+      <View>
+        <Image style={styles.image} source={{ uri: link }} />
+      </View>
     </View>
   );
 }
@@ -108,5 +121,10 @@ const styles = StyleSheet.create({
     shadowOffset: { width: -2, height: 4 },
     shadowOpacity: 0.2,
     shadowRadius: 3,
+  },
+  image: {
+    width: 400,
+    height: 400,
+    resizeMode: 'contain',
   },
 });
